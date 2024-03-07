7a97edecdf0ca2ef22cb22305781963a3322e897
hardik7863 <hardikbatwal@gmail.com>
2024-03-07 12:16:43 +0530

    updating files

--- a022PractiseQuestion.java
+++ a022PractiseQuestion.java
@@ -6,5 +6,61 @@ public class a022PractiseQuestion {
         // int exp2 = (x * (y / x));
         // System.out.println(exp1 + ",");
         // System.out.println(exp2);
+
+        //Ans 5,4
+    
+    
+    //q2
+    //public class Test {
+        // public static void main(String[] args) {
+        // int x = 200, y = 50, z = 100;
+        // if(x > y && y > z){
+        // System.out.println("Hello");
+        // }
+        // if(z > y && z < x){
+        // System.out.println("Java");
+        // }
+        // if((y+200) < x && (y+150) < z){
+        // System.out.println("Hello Java");
+        // }
+
+        //ans Java
+     
+        //Q3
+        // public class Test {
+        //   public class main(String args []){  int x, y, z;
+        //     x = y = z = 2;
+        //     x += y;
+        //     y -= z;
+        //     z /= (x + y);
+        //     System.out.println(x + " " + y + " " + z);
+        // }}
+        //ans 4,0,0
+
+        //Q4
+        // public class Test {
+        //     public static void main(String[] args){
+        //     int x = 9, y = 12;
+        //     int a = 2, b = 4, c = 6;
+        //     int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+        //     System.out.println(exp);
+        //     }
+        //     }
+
+        //ans 278
+
+
+        //Q5
+        // public class Test {
+        //     public static void main(String[] args){
+        //     int x = 10, y = 5;
+        //     int exp1 = (y * (x / y + x / y));
+        //     int exp2 = (y * x / y + y * x / y);
+        //     System.out.println(exp1);
+        //     System.out.println(exp2);
+        //     }
+        //     }
+        
+        // 20, 20
     }
 }
