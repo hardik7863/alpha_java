201a24f2d2de007f097f72fa4b53abc4d99314ea
hardik7863 <hardikbatwal@gmail.com>
2024-03-07 14:38:12 +0530

    uploading files

--- a033PractiseProblem.java
+++ a033PractiseProblem.java
@@ -1,5 +1,82 @@
+import java.util.*;
+
 class a033PractiseProblem {
     public static void main(String[] args) {
+        Scanner sc=new Scanner((System.in));
+
+        //Q1. Writa A Java program to get a number from user and print whether it is positive or negative
+        //  int x=sc.nextInt();
+        //  if (x>0) {
+        //     System.out.println("positive");
+        //  }
+        //  else{
+        //     System.out.println("negative");
+        //  }
         
+
+        //Q2.Finish the following code so that it prints You have a fever if your temperature
+          //is above 100 and otherwise prints You don't have a fever.
+        //   int YrTemp =sc.nextInt();
+        //   if (YrTemp>100) {
+        //     System.out.println("you have fever");
+        //   }
+        //   else{
+        //     System.out.println("you dont have fever");
+        //   }
+
+        
+
+        //Q3.WAP to input week number (1-7)and print day OF WEEK NAME USING SWITCH CASE
+        // System.out.println("enter the week no.");  
+        // int weeekNo =sc.nextInt();
+        //   switch (weeekNo) {
+        //     case 1:
+        //         System.out.println("monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("wednesday");
+        //         break;
+
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;
+
+          
+        //     default:System.out.println("invalid value entered");
+        //         break;}
+        
+    //Q4. What will be the value of x and y in the following program
+        //  int a=63, b=36;
+        //  boolean x=(a<b)?true:false;
+        //  int y =(a>b)?a:b;
+
+        // ans x= false , y=63
+    
+    //Q5. Write a Java program that takes a year from the user and print whether that
+    //year is a leap year or not.
+    
+    // System.out.print("Input the year: ");
+    // int year = sc.nextInt();
+    // boolean x = (year % 4) == 0;
+    // boolean y = (year % 100) != 0;
+    // boolean z = ((year % 100 == 0) && (year % 400 == 0));
+    // if (x && (y || z)) {
+    // System.out.println(year + " is a leap year");
+    // }
+    // else {
+    // System.out.println(year + " is not a leap year");
+// }
     }
 }
